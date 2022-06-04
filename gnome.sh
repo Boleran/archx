fb54aeff9d0e7b7865c306546d0518d9d2790e3c
Boleran <80973401+Boleran@users.noreply.github.com>
2022-06-04 14:50:07 +0700

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -12,7 +12,7 @@ sudo firewall-cmd --reload
 # sudo virsh net-autostart default
 
 
-sudo pacman -S xorg gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+sudo pacman -S xorg gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc
 
 # sudo flatpak install -y spotify
 # sudo flatpak install -y kdenlive
